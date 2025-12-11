e515b41b316952d760caeb543f1c5c6a4d1eaabb
Andor Kesselman <andor@andor.us>
2025-12-11 03:05:39 +0000

    fixed capacity

--- src/lib/eventData.ts
+++ src/lib/eventData.ts
@@ -320,11 +320,10 @@ export const events: Record<string, Event> = {
     ],
     schedule: [],
     details: {
-      contactEmail: 'phil@windley.org',
-      isRegistrationOpen: false,
       registrationUrl: 'https://www.eventbrite.com/e/agentic-internet-workshop-2-tickets-1976356257769?aff=oddtdtcreator',
       contactEmail: 'phil@windley.org',
-
+      capacity: 200,
+      isRegistrationOpen: false
     }
   }
 }
